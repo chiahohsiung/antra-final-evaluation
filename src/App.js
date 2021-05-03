976558e16287be718765f397bc96669f01c1c771
Jacky Hsiung <forlol1123@gmail.com>
2021-05-03 15:30:13 -0400

    router

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Portfolio from './components/Portfolio'
+import { Switch, Route} from "react-router-dom";
+import Contact from './components/Contact';
+
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Portfolio></Portfolio>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
     </div>
   );
 }
