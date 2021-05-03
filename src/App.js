789a130fa4bc3a4b66198b52e6b00f0a499ab2a1
Jacky Hsiung <forlol1123@gmail.com>
2021-05-03 15:18:25 -0400

    main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar'
-
+import Home from './components/Home'
+import Portfolio from './components/Portfolio'
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Portfolio></Portfolio>
     </div>
   );
 }
