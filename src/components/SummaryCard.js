1a944ec887b3e0ba43bcd4c65c1ed01ebdc5c74e
Jacky Hsiung <forlol1123@gmail.com>
2021-05-04 16:46:42 -0400

    change summary card layout

--- src/components/SummaryCard.js
+++ src/components/SummaryCard.js
@@ -4,16 +4,15 @@ function SummaryCard(props) {
   const { title, detail, imagePath } = props
   return (
     <div className="summary-card">
-      <div className="development-summary">
-
-        <div className="development-img">
+      <div className="summary-card-content">
+        <div className="summary-card-img-container text-center">
           <img className="img-responsive" src={imagePath} />
         </div>
+
         <div className="development-description">
           <h3>{title}</h3>
           <p>{detail}</p>
         </div>
-
       </div>
     </div>
   )
