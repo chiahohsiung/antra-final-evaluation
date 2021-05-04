65b501eef281fd6cbdde9cd99566e9d1b17764ae
Jacky Hsiung <forlol1123@gmail.com>
2021-05-04 12:04:05 -0400

    change summaryCard size

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -3,81 +3,35 @@ import developerdesign from "../images/developerdesign.svg";
 import responsivedesign from "../images/responsivedesign.svg";
 import innovativesolutions from "../images/innovativesolutions.svg";
 import passion from "../images/passion.svg";
-import './Summary.css'
 
 import SummaryCard from './SummaryCard'
+import './Summary.css'
+
+
 function Summary() {
-  const descriptions = ['Development and Design', 'Responsive Layouts', 'Ideas and Solutions', 'Passion and Dedication']
+  const titles = ['Development and Design', 'Responsive Layouts', 'Ideas and Solutions', 'Passion and Dedication']
   const imagePaths = [developerdesign, responsivedesign, innovativesolutions, passion]
-  const details = Array(4).fill("Development and design isn't merely putting information on the site or preferred media outlet. I organize content and present information in an engaging fashion, driving new and unique layouts in tandem with novel solutions and cool animations")
-  console.log('details', details)
+  const details = [
+    "I aim to put my creativity to the test, designing and building unique, meaningful products for clients or merely for my own interests.",
+    "Development and design isn't merely putting information on the site or preferred media outlet. I organize content and present information in an engaging fashion, driving new and unique layouts in tandem with novel solutions and cool animations",
+    "There are still many problems that exist in today's society, including laziness. Luckily, I hope to combat these issues by innovating, developing easy-to-use programs, solutions, or products.",
+    "With my profound interest and commitment to my field of study, my projects rarely go unfinished and my problems are never left unresolved."
+  ]
   let cards = []
-  descriptions.forEach((description, index) => {
-    console.log('hi')
-    console.log(details[index])
+  titles.forEach((title, index) => {
     cards.push(
       <SummaryCard
         key={index}
-        description={description}
+        title={title}
         detail={details[index]}
         imagePath={imagePaths[index]}
       />
     )
   })
-  console.log('cards', cards)
+
   return (
     <div className="summary">
       {cards}
-      {/* <div class="container-fluid">
-        <div class="my-row">
-
-
-          <div class="responsive-summary">
-            <div class="col-sm-1 col-md-1 col-sm-offset-2 col-md-offset-2">
-              <div class="responsive-img">
-                <img class="img-responsive" src={responsivedesign}/>
-              </div>
-            </div>
-
-            <div class="col-sm-4 col-md-4">
-              <div class="responsive-description">
-                <h3>Responsive Layouts</h3>
-                <p>Development and design isn't merely putting information on the site or preferred media outlet. I organize content and present information in an engaging fashion, driving new and unique layouts in tandem with novel solutions and cool animations.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="ideas-summary">
-            <div class="col-sm-1 col-md-1">
-              <div class="idea-img">
-                <img class="img-responsive" src={innovativesolutions} />
-              </div>
-            </div>
-
-            <div class="col-sm-4 col-md-4">
-              <div class="idea-description">
-                <h3>Ideas and Solutions</h3>
-                <p>There are still many problems that exist in today's society, including laziness. Luckily, I hope to combat these issues by innovating, developing easy-to-use programs, solutions, or products.</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="passion-summary">
-            <div class="col-sm-1 col-md-1 col-sm-offset-2 col-md-offset-2">
-              <img class="img-responsive" src={passion} />
-            </div>
-
-            <div class="col-sm-4 col-md-4">
-              <div class="passion-description">
-                <h3>Passion and Dedication</h3>
-                <p>With my profound interest and commitment to my field of study, my projects rarely go unfinished and my problems are never left unresolved. </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </div>
   )
 }
--- src/components/SummaryCard.js
+++ src/components/SummaryCard.js
@@ -1,21 +1,18 @@
 import React from 'react'
 import './SummaryCard.css'
 function SummaryCard(props) {
-  const {description, detail, imagePath } = props
+  const { title, detail, imagePath } = props
   return (
     <div className="summary-card">
       <div className="development-summary">
 
-          <div className="development-img">
-            <img className="img-responsive" src={imagePath} />
-          </div>
-
-
-
-          <div className="development-description">
-            <h3>{description}</h3>
-            <p>{detail}</p>
-          </div>
+        <div className="development-img">
+          <img className="img-responsive" src={imagePath} />
+        </div>
+        <div className="development-description">
+          <h3>{title}</h3>
+          <p>{detail}</p>
+        </div>
 
       </div>
     </div>
