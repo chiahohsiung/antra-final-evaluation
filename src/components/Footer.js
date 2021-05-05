84d1285231c630d0c5da2370d0b1f1bbd1c9a477
Jacky Hsiung <forlol1123@gmail.com>
2021-05-04 22:31:50 -0400

    footer flexblox

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -34,17 +34,14 @@ function Footer() {
           {/* </div> */}
         </div>
 
-        <div className="row">
-          <div className="col-sm-6 col-md-6 text-right">
-            <div className="personal-contact phone">
-              <h4>Phone: XXXXXX</h4>
-            </div>
+        <div className="personal-contact-container">
+          <div className="personal-contact phone">
+            <h4>Phone: XXXXXX</h4>
           </div>
 
-          <div className="col-sm-6 col-md-6 text-left">
-            <div className="personal-contact email-personal-contact">
-              <h4>Email: XXXXX@gmail.com</h4>
-            </div>
+
+          <div className="personal-contact email-personal-contact">
+            <h4>Email: XXXXX@gmail.com</h4>
           </div>
         </div>
 
